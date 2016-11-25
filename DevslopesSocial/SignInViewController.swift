95ed979603a978e3ca0e354ab17ab264e878bf5f
Roberto Montesinos <Tico@Robertos-iMac.local>
2016-11-25 11:59:25 -0800

    completed setting up firebase integration

---
+++ DevslopesSocial/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevslopesSocial
+//
+//  Created by Roberto Montesinos on 11/25/16.
+//  Copyright © 2016 Roberto Montesinos. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DevslopesSocial' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DevslopesSocial
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
+  
+
+  target 'DevslopesSocialTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
